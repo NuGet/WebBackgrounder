ebcc9993ee60ada0822c9ee57050e6ae7df58ded
Haacked <haacked@gmail.com>
2011-10-14 14:30:36 -0700

    Enabled package build

--- build-packages.bat
+++
@@ -1,2 +0,0 @@
-src\.nuget\nuget pack src\WebBackgrounder\WebBackgrounder.csproj -Symbols
-src\\.nuget\nuget pack src\WebBackgrounder.EntityFramework\WebBackgrounder.EntityFramework.csproj -Symbols	
