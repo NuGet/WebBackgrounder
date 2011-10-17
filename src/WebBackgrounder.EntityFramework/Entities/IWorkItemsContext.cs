30c6eb4efc52766755ab1180ff8d66642658ab0d
Haacked <haacked@gmail.com>
2011-10-17 14:32:59 -0700

    Adding IWorkITemContex

---
+++ src/WebBackgrounder.EntityFramework/Entities/IWorkItemsContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebBackgrounder.EntityFramework.Entities
+{
+    public interface IWorkItemsContext : IDisposable
+    {
+        IDbSet<WorkItem> WorkItems { get; set; }
+        int SaveChanges();
+    }
+}
